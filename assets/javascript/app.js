9790c34c132cfb2cacd327f21342c8d4c480819b
PTMcEneaney <pagemceneaney@gmail.com>
2019-05-04 18:31:50 -0700

    add base files and folders

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDdZODkoCeaRFptBbnBb63tKBxQzZILnZ8",
+    authDomain: "train-scheduler-346ff.firebaseapp.com",
+    databaseURL: "https://train-scheduler-346ff.firebaseio.com",
+    projectId: "train-scheduler-346ff",
+    storageBucket: "train-scheduler-346ff.appspot.com",
+    messagingSenderId: "433471485719",
+    appId: "1:433471485719:web:7c35fbb9cbb7eb5d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
