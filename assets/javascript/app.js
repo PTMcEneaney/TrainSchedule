aa3525b87acb411af243388d3145bc9e81b6ea79
PTMcEneaney <pagemceneaney@gmail.com>
2019-05-05 21:02:38 -0700

    add functionality and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,6 @@
-
-
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+$( document ).ready( function() {
+    // Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyDdZODkoCeaRFptBbnBb63tKBxQzZILnZ8",
     authDomain: "train-scheduler-346ff.firebaseapp.com",
     databaseURL: "https://train-scheduler-346ff.firebaseio.com",
@@ -13,3 +11,68 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+
+$('#submitBtn').on("click", function () {
+
+    var name = $('#name').val().trim();
+    var destination = $('#destination').val().trim();
+    var time = $('#time').val().trim();
+    var frequency = $('#frequency').val().trim();
+
+    database.ref().push({
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP,
+    });
+
+});
+
+
+
+database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", function (childSnapshot) {
+
+    var sv = childSnapshot.val();
+    var name = sv.name;
+    var destination = sv.destination;
+    var time = sv.time;
+    var frequency = sv.frequency;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(time, "HH:mm").subtract(1, "years");
+    // console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    // console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+
+
+    var dataSet = ("<tr><td>" + name + "</td><td>" + destination + "</td><td>" +
+        frequency + "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + tMinutesTillTrain + "</td></tr>");
+
+    $("#tbody").append(dataSet);
+});
+
+
+
+});
